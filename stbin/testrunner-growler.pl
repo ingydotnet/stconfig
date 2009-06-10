25c4cf199ebaab6001bf1525b363118fa9340da0
Stash <stash@aedion.net>
2009-06-10 16:00:03 -0700

    Script to growl testrunner results

---
+++ stbin/testrunner-growler.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+use Mac::Growl ':all';
+use YAML;
+use LWP::Simple;
+
+my $branch = shift || 'master';
+
+my @notifications = qw(test);
+
+RegisterNotifications("Testrunner", \@notifications, \@notifications);
+
+my $yaml = get("http://testrunner.socialtext.net/stci/$branch/in-progress/publishing-vars.yaml");
+my $parsed = Load($yaml);
+
+my $passed = $parsed->{overall_results}{passed};
+my $failed = $parsed->{overall_results}{failed};
+
+PostNotification("Testrunner", 'test', "Test Results for $branch", "$passed Passed, $failed Failed");
