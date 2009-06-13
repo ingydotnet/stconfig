710a7be60f089e0d9d432638f3f02ad1e634d68e
Stash <stash@aedion.net>
2009-06-12 18:52:33 -0700

    Make testrunner growler show priority, more tolerant of errors

--- stbin/testrunner-growler.pl
+++ stbin/testrunner-growler.pl
@@ -13,9 +13,25 @@ my @notifications = qw(test);
 RegisterNotifications("Testrunner", \@notifications, \@notifications);
 
 my $yaml = get("http://testrunner.socialtext.net/stci/$branch/in-progress/publishing-vars.yaml");
+exit unless $yaml;
 my $parsed = Load($yaml);
+exit unless $parsed;
 
 my $passed = $parsed->{overall_results}{passed};
 my $failed = $parsed->{overall_results}{failed};
 
-PostNotification("Testrunner", 'test', "Tests on $branch", "$passed Passed, $failed Failed");
+# You can configure some growl styles to make the messages a different color
+# based on the priority level
+# -2 = very low (all tests passing)
+# -1 = moderate
+#  0 = normal
+#  1 = high (some failures)
+#  2 = emergency (> 2% failing after 4000 tests)
+
+my $prio = -2;
+$prio = 1 if $failed;
+$prio = 2 if ($failed/$passed > 0.02 && $passed > 4000);
+
+my $sticky = 0;
+
+PostNotification("Testrunner", 'test', "Tests on $branch", "$passed Passed, $failed Failed", $sticky, $prio);
