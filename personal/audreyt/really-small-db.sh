abc650a8ef191582993d9c9fab31441e5dcee97b
Audrey Tang <audrey.tang@socialtext.com>
2010-02-18 18:58:54 -0800

    * Tiny fix to get really-small-db running in any pwd.

--- personal/audreyt/really-small-db.sh
+++ personal/audreyt/really-small-db.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-cdnlw
-perl dev-bin/really-big-db.pl -a 2 -u 10 -p 20 -e 50 -g 20 --group-ws-ratio 1 --group-user-ratio 3 --view-event-ratio 0 -s 100
+cd $HOME/src/st/socialtext/nlw && perl dev-bin/really-big-db.pl -a 2 -u 1024 -p 20 -e 50 -g 20 --group-ws-ratio 1 --group-user-ratio 3 --view-event-ratio 0 -s 100
